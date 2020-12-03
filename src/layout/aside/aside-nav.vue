63b9e9e5195bd1941b593073476252e4dc36cfa2
lm.mo <jasminemei@163.com>
2020-12-03 16:06:00 +0800

    layout准备

---
+++ src/layout/aside/aside-nav.vue
@@ -0,0 +1,67 @@
+<template>
+  <el-menu
+    default-active="2"
+    class="el-menu-vertical-demo"
+    background-color="#545c64"
+    text-color="#fff"
+    active-text-color="#ffd04b"
+    :collapse="isCollapse"
+    @open="handleOpen"
+    @close="handleClose"
+  >
+    <el-submenu index="1">
+      <template slot="title">
+        <i class="el-icon-location" />
+        <span slot="title">导航一</span>
+      </template>
+      <el-menu-item-group>
+        <span slot="title">分组一</span>
+        <el-menu-item index="1-1">选项1</el-menu-item>
+        <el-menu-item index="1-2">选项2</el-menu-item>
+      </el-menu-item-group>
+      <el-menu-item-group title="分组2">
+        <el-menu-item index="1-3">选项3</el-menu-item>
+      </el-menu-item-group>
+      <el-submenu index="1-4">
+        <span slot="title">选项4</span>
+        <el-menu-item index="1-4-1">选项1</el-menu-item>
+      </el-submenu>
+    </el-submenu>
+    <el-menu-item index="2">
+      <i class="el-icon-menu" />
+      <span slot="title">导航二</span>
+    </el-menu-item>
+    <el-menu-item index="3">
+      <i class="el-icon-document" />
+      <span slot="title">导航三</span>
+    </el-menu-item>
+    <el-menu-item index="4">
+      <i class="el-icon-setting" />
+      <span slot="title">导航四</span>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script>
+export default {
+  props: {
+    isCollapse: {
+      type: Boolean,
+      default: true
+    }
+  },
+  data() {
+    return {}
+  },
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped></style>
---
+++ src/layout/index.vue
@@ -0,0 +1,74 @@
+<template>
+  <el-container>
+    <i
+      class="icons"
+      :class="isCollapse ? 'el-icon-s-unfold' : 'el-icon-s-fold'"
+      @click="isCollapse = !isCollapse"
+    />
+    <el-header>
+      <Nav />
+    </el-header>
+    <el-container>
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <AsideNav :is-collapse="isCollapse" />
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+import Nav from './header/nav'
+import AsideNav from './aside/aside-nav'
+export default {
+  components: { Nav, AsideNav },
+  data() {
+    return {
+      isCollapse: true
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.el-container {
+  position: absolute;
+  width: 100%;
+  top: 0px;
+  left: 0;
+  bottom: 0;
+}
+.el-header {
+  padding: 0;
+  z-index: 1000;
+}
+
+.el-header .el-menu {
+  border-bottom: none;
+}
+.el-aside,
+.el-main {
+  padding-top: 60px;
+}
+.el-aside {
+  background: #545c64;
+}
+.el-aside .el-menu {
+  border-right: none;
+}
+.el-icon-s-unfold:before,
+.el-icon-s-fold:before {
+  position: fixed;
+  top: 75px;
+  font-size: 30px;
+  z-index: 464564;
+}
+.el-icon-s-unfold:before {
+  transition-duration: 0.2s;
+  left: 64px;
+}
+.el-icon-s-fold:before {
+  transition-duration: 0.5s;
+  left: 200px;
+}
+</style>
